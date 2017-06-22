9ebdc9d2203c72ed5084f085e1ff193f1d3c2abb
tricialeach <tleach@gmail.com>
2017-06-22 12:38:15 -0700

    Encrypted text will display in textarea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 from caesar import rotate_string
-from test import hey
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,19 +9,19 @@ form = """<!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -31,7 +30,7 @@ form = """<!DOCTYPE html>
         <input type="text" name="rot">
       </label>
         <br>
-        <textarea name="text"></textarea>
+        <textarea name="text">{0}</textarea>
         <input type="submit">
       </form>
     </body>
@@ -40,7 +39,7 @@ form = """<!DOCTYPE html>
 
 @app.route("/")
 def index():
-    return form
+    return form.format(...)
 
 @app.route("/", methods=['POST'])
 def encrypt():
@@ -49,7 +48,7 @@ def encrypt():
     text_from_form = request.form["text"]
     encrypted = rotate_string(text_from_form,int_rot)
 
-    return "<h1> " + encrypted + "</h1>"
+    return form.format(encrypted)
     #return str(int_rot)
 
 app.run()
